07fcd7e18a9493943536828b34d427458b3d8934
0918nobita <nobita.0918@gmail.com>
2016-09-20 15:28:53 +0900

    履歴に名前も保存するようにした

--- 活動記録システム/MainWindow.xaml.cs
+++ 活動記録システム/MainWindow.xaml.cs
@@ -52,7 +52,7 @@ namespace 活動記録システム
                     if (!line.Equals(""))
                     {
                         string[] cells = line.Split(',');
-                        Collection.Insert(0, new Activity { Date = cells[0], Title = cells[1], Content = cells[2] });
+                        Collection.Insert(0, new Activity { Date = cells[0], Name = cells[1], Title = cells[2], Content = cells[3] });
                     }
                 }
             }
@@ -82,7 +82,7 @@ namespace 活動記録システム
             if (date.Text.Equals("")) { MessageBox.Show("日付が選択されていません"); return; }
             if (title.Text.Equals("")) { MessageBox.Show("タイトルが入力されていません"); return; }
             if (content.Text.Equals("")) { MessageBox.Show("内容が入力されていません"); return; }
-            File.AppendAllText(@path, date.Text + "," + title.Text + "," + content.Text + "\n");
+            File.AppendAllText(@path, date.Text + "," + MemberCollection[comboBox.SelectedIndex].Name + "," + title.Text + "," + content.Text + "\n");
             updateHistory();
         }
 
