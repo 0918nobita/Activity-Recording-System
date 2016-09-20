f90cf8065ffbf424bb8680c1c1bfc6ea77a5a144
0918nobita <nobita.0918@gmail.com>
2016-09-20 15:06:08 +0900

    部員一覧をmember.txtに保存するようにした
    updateMemberメソッドで部員一覧を更新する

--- 活動記録システム/MainWindow.xaml.cs
+++ 活動記録システム/MainWindow.xaml.cs
@@ -10,11 +10,13 @@ namespace 活動記録システム
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Person> MemberCollection { get; set; }
         public ObservableCollection<Activity> Collection { get; set; }
         private string @path = "./activity.csv";
 
         public MainWindow()
         {
+            MemberCollection = new ObservableCollection<Person>();
             Collection = new ObservableCollection<Activity>();
             InitializeComponent();
 
@@ -23,6 +25,12 @@ namespace 活動記録システム
                 using (FileStream fileStream = File.Create(path)) { }
             }
 
+            if (!System.IO.File.Exists("./member.txt"))
+            {
+                using (FileStream fileStream = File.Create("./member.txt")) { }
+            }
+
+            updateMember();
             updateHistory();
         }
 
@@ -50,8 +58,27 @@ namespace 活動記録システム
             }
         }
 
+        private void updateMember()
+        {
+            MemberCollection.Clear();
+            string csvText;
+            using (StreamReader streamReader = new StreamReader("./member.txt", Encoding.GetEncoding("UTF-8")))
+            {
+                csvText = streamReader.ReadToEnd();
+            }
+
+            using (StringReader stringReader = new StringReader(csvText))
+            {
+                while (stringReader.Peek() > -1)
+                {
+                    MemberCollection.Add(new Person { Name = stringReader.ReadLine() });
+                }
+            }
+        }
+
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            if (comboBox.Text.Equals("")) { MessageBox.Show("名前が選択されていません"); return; }
             if (date.Text.Equals("")) { MessageBox.Show("日付が選択されていません"); return; }
             if (title.Text.Equals("")) { MessageBox.Show("タイトルが入力されていません"); return; }
             if (content.Text.Equals("")) { MessageBox.Show("内容が入力されていません"); return; }
@@ -73,12 +100,25 @@ namespace 活動記録システム
         {
             MessageBox.Show("活動記録システム Version 1.0\n対象フレームワーク .NET Framework 4.5.2\n開発者 0918nobita");
         }
+        private void add_member_Click(object sender, RoutedEventArgs e)
+        {
+            if (member_name.Text.Equals("")) { MessageBox.Show("名前が入力されていません"); return; }
+            File.AppendAllText("./member.txt", member_name.Text + "\n");
+            updateMember();
+            member_name.Text = "";
+        }
     }
 
     public class Activity
     {
         public string Date { get; set; }
+        public string Name { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
     }
 }
